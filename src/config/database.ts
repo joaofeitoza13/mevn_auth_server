c2f58d28fa753712d6e4d09dccdbae3fc4adde4c
João Feitoza <joaofeitoza.13@gmail.com>
2023-05-01 17:13:21 -0300

    feat: connect to Mongo Atlas via Mongoose

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import mongoose, { ConnectOptions } from 'mongoose'
+
+export const connectDB = async (db: string) => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    } as ConnectOptions)
+  } catch (error) {
+    console.log(error)
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import { connectDB } from './config/database'
 
 dotenv.config()
-
-const port = process.env.PORT
-
+const port = process.env.PORT!
+// const database: string = process.env.LOCAL_DB
+const database: string = process.env.REMOTE_DB!
 const server: Express = express()
+
+connectDB(database)
 
 server.get('/', (req: Request, res: Response) => {
   res.send("Typescript Express Server's root.")
@@ -15,6 +19,13 @@ server.all('*', (req: Request, res: Response) => {
   res.sendStatus(404)
 })
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+mongoose.connection.once('open', () => {
+  if(database === process.env.REMOTE_DB) {
+    console.log(`Connected to MongoDB remotely.`)
+  } else if (database === process.env.LOCAL_DB) {
+    console.log(`Connected to MongoDB locally.`)
+  }
+  server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+  })
 })
