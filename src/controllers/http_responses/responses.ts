96d2febb877a503ee205620b0e75ae58941228e3
João Feitoza <joaofeitoza.13@gmail.com>
2023-05-02 01:10:26 -0300

    refactor: move return to the controller and organize in ascending http code

--- src/controllers/http_responses/responses.ts
+++ src/controllers/http_responses/responses.ts
@@ -1,31 +1,35 @@
 import { Response } from "express"
 
+export const ok = (res: Response,  message: string) => {
+  res.status(200).json({ message: message })
+}
+
 export const created = (res: Response, message: string) => {
   res.status(201).json({ message: message })
+}
+
+export const noContent = (res: Response,  message: string) => {
+  res.status(203).json({ message: message })
 }
 
 export const badRequest = (res: Response, message: string) => {
   res.status(400).json({ message: message })
 }
 
-export const forbidden = (res: Response,  message: string) => {
-  res.status(403).json({ message: message })
+export const unauthorized = (res: Response,  message: string) => {
+  res.status(401).json({ message: message })
 }
 
-export const conflict = (res: Response,  message: string): Response => {
-  return res.status(409).json({ message: message })
+export const forbidden = (res: Response,  message: string) => {
+  res.status(403).json({ message: message })
 }
 
 export const notFound = (res: Response,  message: string) => {
   res.status(404).json({ message: message })
 }
 
-export const unauthorized = (res: Response,  message: string) => {
-  res.status(401).json({ message: message })
-}
-
-export const noContent = (res: Response,  message: string) => {
-  res.status(203).json({ message: message })
+export const conflict = (res: Response,  message: string) => {
+  res.status(409).json({ message: message })
 }
 
 export const internalServerError = (res: Response,  message: string) => {
